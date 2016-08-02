b701c509a8c0ce5300eec78646919861c9fe328a
Jedidiah Carlson <jedidiah@umich.edu>
2016-08-02 10:30:42 -0400

    Tue Aug  2 10:30:42 2016: script in shiny dir

--- server.R
+++ server.R
@@ -15,7 +15,7 @@ library(ggplot2)
 library(Cairo)   # For nicer ggplot2 output when deployed on Linux
 library(DT)
 library(RColorBrewer)
-setwd("/var/www/jedidiahcarlson.com")
+# setwd("/var/www/jedidiahcarlson.com")
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output, session) {
@@ -44,7 +44,7 @@ shinyServer(function(input, output, session) {
     # filepath<- inFile$datapath
     inpath <- infile()$filepath
     adj <- input$adj
-    processcmd <- paste0("perl cgi/mr_eel.pl --in ", inpath, " --adj ", adj)
+    processcmd <- paste0("perl mr_eel.pl --in ", inpath, " --adj ", adj)
     if(input$seq){
       processcmd <- paste0(processcmd, " --seq")
     }
