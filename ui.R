ed7f62acb4ab090dabd4b260e1da7a6173640778
Jedidiah Carlson <jedidiah@umich.edu>
2016-08-02 12:54:34 -0400

    Tue Aug  2 12:54:33 2016: update documentation

--- ui.R
+++ ui.R
@@ -8,7 +8,7 @@ library(shinyjs)
 
 textInputRow<-function (inputId, label, value = "") {
   div(style="display:inline-block;",
-      tags$label(label, `for` = inputId), 
+      tags$label(label, `for` = inputId),
       tags$input(id = inputId, type = "text", value = value, class="input-small"))
 }
 
@@ -21,37 +21,37 @@ shinyUI(fluidPage(
         sidebarPanel(
           fileInput('file1', 'Choose file'),
           tags$hr(),
-          selectInput("adj", label="Motif length", 
+          selectInput("adj", label="Motif length",
                       choices=list("7-mers"=3, "5-mers"=2), selected=3),
           checkboxInput('seq', 'Include sequence motif?', FALSE),
-          
+
           # textInputRow(inputId="scale", label="Scale rates to:", value = 0),
-          div(id="scaling", "Scale rates to:", 
-              textInputRow(inputId="scale", label="", value = 0), 
-              HTML(paste("x10", tags$sup(-8), 
+          div(id="scaling", "Scale rates to:",
+              textInputRow(inputId="scale", label="", value = 0),
+              HTML(paste("x10", tags$sup(-8),
                          " (keep value as 0 to return relative rates)", sep = ""))),
-          shinyjs::hidden(div(id="scibox", 
+          shinyjs::hidden(div(id="scibox",
                           checkboxInput('sci', 'Output in scientific notation?', FALSE))),
           actionButton("submit", "Submit")
         ),
-    
+
         mainPanel(
           tabsetPanel(
             tabPanel("View output",
                   DT::dataTableOutput("output"),
                   tags$hr(),
                   downloadButton('downloadData', 'Download Processed Data')),
-            tabPanel("Plots", 
+            tabPanel("Plots",
                      plotOutput("muPlot"))
           )
         )
-    
+
       )
-  
+
     ),
-    
-    tabPanel("Documentation", 
+
+    tabPanel("Documentation",
              includeMarkdown("/srv/shiny-server/mr-eel/mr-eel.md"))
-             # includeMarkdown("mr-eel.md"))
+    )
   )
 ))
